e68f6d8de6968b946ac3283d92c952c7cf0c81f2
Ledina Rrethi <ledinarrethi@gmail.com>
2024-10-30 13:35:28 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          textAlign: 'center',
+          alignItems: 'center',
+          height: '100vh',
+        }}
+      >
+        <h1>Setting Up My E-Commerce App </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
